13cbc45494b4ce7477f8c1dd5f4df11f07857c29
Kristopher Hankey <kasdiel@outlook.com>
2020-10-22 23:20:52 -0400

    added name field to gameobject

--- OOP3200-F2020-Lesson3c/main.cpp
+++ OOP3200-F2020-Lesson3c/main.cpp
@@ -1,5 +1,6 @@
 #include <iomanip>
 #include <iostream>
+#include <map>
 
 #include <vector>
 
@@ -51,7 +52,18 @@ static void CompareGameObjects(GameObject* object1, GameObject* object2)
 
 int main()
 {
-	std::vector<GameObject*> gameObjects;
+	// map is made up of key-value pairs - in this case the key is a string
+	// and the value is a GameObject
+	std::map<std::string, GameObject> gameObjects;
+
+	
+
+
+
+
+
+	
+	/*std::vector<GameObject*> gameObjects;
 
 	int num_of_GO;
 	std::cout << "How Many Game Objects do you need?: ";
@@ -71,6 +83,6 @@ int main()
 	std::cout << "\n--------------------------------------------------------------" << std::endl;
 	
 	CompareGameObjects(gameObjects[index1], gameObjects[index2]);
-	CompareGameObjects(gameObjects[index1], gameObjects[index2]);
+	CompareGameObjects(gameObjects[index1], gameObjects[index2]);*/
 }
 
