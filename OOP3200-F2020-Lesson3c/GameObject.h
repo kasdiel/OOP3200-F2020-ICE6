0eb2314ac527754ec17b72bc61e85a1ee6b54405
Kristopher Hankey <kasdiel@outlook.com>
2020-10-22 23:40:41 -0400

    finalized

--- OOP3200-F2020-Lesson3c/GameObject.h
+++ OOP3200-F2020-Lesson3c/GameObject.h
@@ -2,6 +2,7 @@
 #ifndef __GAME_OBJECT__
 #define __GAME_OBJECT__
 
+#include <sstream>
 #include <string>
 
 #include "Vector2D.h"
@@ -13,8 +14,8 @@ public:
 	GameObject();
 	GameObject(int id, float x, float y);
 	GameObject(int id, const Vector2D<float>& position);
-	GameObject(std::string name, int id, float x, float y);
-	GameObject(std::string name, int id, const Vector2D<float>& position);
+	GameObject(const std::string& name, int id, float x, float y);
+	GameObject(const std::string& name, int id, const Vector2D<float>& position);
 
 	// Rule of Three
 	~GameObject(); // Destructor
@@ -34,7 +35,26 @@ public:
 
 	// Utility Functions
 	std::string ToString() const;
-	
+	std::string ToFile() const;
+
+	//friend std::ifstream& operator>>(std::ifstream* in, GameObject& rhs)
+	//{
+	//	int id;
+
+	//	in.  ((char*)&rhs.m_id, sizeof(rhs.m_id));
+	//	in >> id;
+
+
+	//	
+	//	/*in.ignore();
+	//	in >> rhs.m_name;
+	//	in.ignore();
+	//	in >> rhs.m_position;
+	//	in.ignore();*/
+
+	//	return in;
+	//}
+	//
 private:
 	int m_id{};
 	std::string m_name;
